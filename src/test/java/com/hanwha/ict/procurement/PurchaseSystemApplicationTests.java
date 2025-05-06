0dcea3fa97a155a4322e0c3e6fd87d2d2deca3e8
JoungWoo Lee <j_ra1n@naver.com>
2025-05-06 15:17:20 +0900

    Redis test

--- src/test/java/com/hanwha/ict/procurement/PurchaseSystemApplicationTests.java
+++ src/test/java/com/hanwha/ict/procurement/PurchaseSystemApplicationTests.java
@@ -1,9 +1,10 @@
 package com.hanwha.ict.procurement;
 
+import com.hanwha.ict.procurement.controller.PriceController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PriceController.class)
 class PurchaseSystemApplicationTests {
 
 	@Test
