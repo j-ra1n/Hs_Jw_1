e4efbaafb74df5417952be2afd3103dccd12f925
JoungWoo Lee <j_ra1n@naver.com>
2025-05-06 15:46:18 +0900

    Redis test

--- src/main/java/com/hanwha/ict/procurement/PurchaseSystemApplication.java
+++ src/main/java/com/hanwha/ict/procurement/PurchaseSystemApplication.java
@@ -1,15 +1,28 @@
 package com.hanwha.ict.procurement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableCaching
 public class PurchaseSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PurchaseSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseSystemApplication.class, args);
+    }
 
+
+    @Bean
+    public CommandLineRunner testRedis(StringRedisTemplate redisTemplate) {
+        return args -> {
+            String pong = Objects.requireNonNull(redisTemplate.getConnectionFactory()).getConnection().ping();
+            System.out.println("✅ Redis 연결 테스트 결과: " + pong);
+        };
+    }
 }
